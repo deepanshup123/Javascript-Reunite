2e9a0912d562e550a240bca75936cb0ead615924
deepanshup123 <149324354+deepanshup123@users.noreply.github.com>
2026-05-26 11:23:46 +0000

    Recap of DataTypes

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // Treat JS code as newer version
+
+// alert("Hello Dipanshu") // This work in browser but now we Using the node.js then it will not work
+
+// Code readability is key factor in coding, in way of future transparancy as well as easy to read for anyone
+
+let name = "Dipanshu" // String
+let age = 25  // number
+let isLoggedIn = true // Boolean
+
+// number ==> 2 to power 53
+// BigInt
+// String ==> "" / ''
+// Boolean ==> true/false
+// Null ==> Standalone Value
+// Undefined ==> value not provide to any variable
+// Synbol ==> unique
+
+
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof Object); // function
+
