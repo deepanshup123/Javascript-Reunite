6ecd3b22d01ba75aceb3f370aa03178ee558ef20
deepanshup123 <149324354+deepanshup123@users.noreply.github.com>
2026-06-24 13:09:00 +0000

    Conversion and Data types

---
+++ 01_Basic/05_comperision.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+
+// When we compare any thing then make sure that both the values have same data type.
+
+// console.log("2" > 1);
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // True
+
+// when we compare two value with different type then the reslut is not predictable some time as shown in above three example.
+
+//  ** We have learn about the comparision check and equality check
+
+// "===" is used to check the strict means value as well as data types
---
+++ 01_Basic/DataType-Summary.js
@@ -0,0 +1,47 @@
+// Primitve Data Types
+
+/**
+ * 1. Stirng
+ * 2. Number
+ * 3. Boolean
+ * 4. null
+ * 5. undefined
+ * 6. Symbol (Make Unique Value)
+ * 7. BigInt
+ */
+
+// Non Primitve Data Types (Reference Type)
+
+/**
+ * 1. Array
+ * 2. Object
+ * 3. Function
+ */
+
+
+// Javascript is Statically or Dynamic Type Language
+
+// const Id = Symbol("123")
+// const AnotherId = Symbol("123")
+
+// console.log(Id === AnotherId);
+
+
+// const bigNumber = 9324816563806045255n
+
+// console.log(typeof bigNumber);
+
+const heros = ["shaktiman", "nagaraj", "spiderman"]
+
+const MyDetails = {
+    name : "Dipanshu",
+    age : 26,
+    number: 9324816563
+}
+
+
+const MyFunction = function () {
+    console.log("Hello Dipanshu");
+}
+
+MyFunction()
